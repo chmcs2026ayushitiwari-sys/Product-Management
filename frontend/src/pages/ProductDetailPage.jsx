2601421fb9bdeae884330410242e4f843da9fb61
chmcs2026ayushitiwari-sys <chmcs2026.ayushi.tiwari@gmail.com>
2026-03-10 20:11:06 +0530

    Update ProductDetailPage.jsx

--- frontend/src/pages/ProductDetailPage.jsx
+++ frontend/src/pages/ProductDetailPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../axios";
+import api from "../lib/axios";
 import { FiArrowLeft, FiPackage } from "react-icons/fi";
 
 const ProductDetailPage = () => {
